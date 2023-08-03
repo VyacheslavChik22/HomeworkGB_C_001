e315931eff2b8231f408e0960b672a012a6f002e
Vyacheslav <asyofka@mail.ru>
2023-08-03 19:14:54 +0300

    task_03

---
+++ task_04/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+Console.WriteLine("Введите целое число!");
+int introduce = int.Parse(Console.ReadLine()!);
+int number = 1;
+
+
+if (introduce % 2 != 0)
+{
+    while (number != introduce + 1)
+    {
+        if (number % 2 == 0) Console.Write($"{number}");
+        if (number % 2 == 0 && number != introduce - 1) Console.Write(", ");
+
+        number++;
+    }
+}
+else
+{
+while (number != introduce + 1)
+    {
+        if (number % 2 == 0) Console.Write($"{number}");
+        if (number % 2 == 0 && number != introduce) Console.Write(", ");
+
+        number++;
+    }
+}
